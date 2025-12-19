8c49c23b52e0635ad959c90ad6fe94b34abc91bf
webmiko <webmiko@icloud.com>
2025-12-20 00:46:49 +0300

    feat: добавлены импорты классов Category и Product в основной файл
    - Импортированы классы Category и Product из модуля src.
    - Исправлен формат строки в условии запуска основного блока на использование двойных кавычек.
    - Добавлена запятая в список продуктов при создании категории.

--- 15.1_main.py
+++ 15.1_main.py
@@ -1,4 +1,7 @@
-if __name__ == '__main__':
+from src.category import Category
+from src.product import Product
+
+if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
@@ -10,7 +13,7 @@ if __name__ == '__main__':
     category1 = Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3]
+        [product1, product2, product3],
     )
 
     print(str(category1))
