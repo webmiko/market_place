cb9efcc8f296cb7b611fc592cd089101551d35fb
webmiko <webmiko@icloud.com>
2025-12-27 01:21:21 +0300

    feat: добавлены импорты классов Category, LawnGrass и Smartphone в основной файл

--- 16.1_main.py
+++ 16.1_main.py
@@ -1,3 +1,6 @@
+from src.category import Category
+from src.product import LawnGrass, Smartphone
+
 if __name__ == '__main__':
     smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
                          "S23 Ultra", 256, "Серый")
